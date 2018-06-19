3eb4b65e2fbcd51231a241576a34c5576f2da91f
Xavier Lange <xrlange@gmail.com>
2018-06-19 01:15:20 -0400

    Telegraf can read the WAL/data vols and gather debug stats

--- telegraf-image/docker-entrypoint.sh
+++ telegraf-image/docker-entrypoint.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
 set -ex
-export FULL_DISK=$(df | awk '/data/ {print $2 " " $1}' | sort -rn | head -n 1 | awk '{print $2}')
-export DISK=${FULL_DISK/\/dev\//}
+export DATA_DISK_PATH=$(df | awk '/data/ {print $2 " " $1}' | sort -rn | head -n 1 | awk '{print $2}')
+export DATA_DISK_DEV=${DATA_DISK_PATH/\/dev\//}
+
+export WAL_DISK_PATH=$(df | awk '/wal/ {print $2 " " $1}' | sort -rn | head -n 1 | awk '{print $2}')
+export WAL_DISK_DEV=${WAL_DISK_PATH/\/dev\//}
 
 exec telegraf --debug
